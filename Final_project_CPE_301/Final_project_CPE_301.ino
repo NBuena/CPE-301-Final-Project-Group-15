62a5acff213f856e484edc3acc56a8c91dd8f5f2
NBuena <nbuenaventura@unr.edu>
2024-12-13 15:46:27 -0800

    added rudimentary USART functionality

--- Final_project_CPE_301/Final_project_CPE_301.ino
+++ Final_project_CPE_301/Final_project_CPE_301.ino
@@ -1,21 +1,30 @@
-By: Gali Hacohen, Noah Buenaventura
+//By: Gali Hacohen, Noah Buenaventura
 
 #include <DHT.h>
-#include <LiquidCrystal_I2C.h>
+#include <TinyLiquidCrystal_I2C.h>
+
 
 //POINTERS
 
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+#define RDA 0x80
+#define TBE 0x20  
 #define DHTPIN 2        
 #define DHTTYPE DHT22
       
 // Initialize the DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+TinyLiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 // LCD STUFF 
 void setup() {
+  U0init(9600);
   dht.begin();
   
 
@@ -51,4 +60,58 @@ void loop() {
   lcd.print("%");
 
   delay(1000);
+  //USART EXAMPLE CODE -----
+  /*
+  unsigned char cs1;
+  while (U0kbhit()==0){}; // wait for RDA = true
+  cs1 = U0getchar();    // read character
+  U0putchar(cs1);     // echo character
+  */
+}
+//
+// function to initialize USART0 to "int" Baud, 8 data bits,
+// no parity, and one stop bit. Assume FCPU = 16MHz.
+//
+void U0init(unsigned long U0baud)
+{
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+//
+// Read USART0 RDA status bit and return non-zero true if set
+//
+unsigned char U0kbhit()
+{
+  if(*myUCSR0A & (1<<7)){
+    return 1;
+  }else {
+    return 0;
+  }
+}
+//
+// Read input character from USART0 input buffer
+//
+unsigned char U0getchar()
+{
+  return *myUDR0;
+  /*
+   unsigned char fly;
+   fly = *myUDR0;
+   return fly;
+  */
+}
+//
+// Wait for USART0 (myUCSR0A) TBE to be set then write character to
+// transmit buffer
+//
+void U0putchar(unsigned char U0pdata)
+{
+  while(!( *myUCSR0A  & (1 << 5)));
+  *myUDR0  = U0pdata;
 }

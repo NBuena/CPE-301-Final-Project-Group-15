ef12bbad10919e4e14bf947881c8310e561c51df
SlappingCheese <gahacohen@gmail.com>
2024-12-13 16:03:15 -0800

    Add files via upload
     this has the led stuff that can help

--- final_project.ino
+++ final_project.ino
@@ -2,14 +2,40 @@ By: Gali Hacohen, Noah Buenaventura
 
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
-# include <stepper.h>
+#include <stepper.h>
+#include <ezLED.h>
 
 //POINTERS
+
+// LED's
+#define LED_yellow pin 9
+#define LED_blue pin 8
+#define LED_red pin 7
+#define LED_green pin 6
+
+// Stepper motor
 #define stepper_revs (2038)
+
+// Temprature
 #define DHTPIN 2        
 #define DHTTYPE DHT22
 
-// Initialize the stepper motor
+// Initialize the LED's
+void steup(){
+  pinMode(LED_yellow, OUTPUT);
+  pinMode(LED_blue, OUTPUT);
+  pinMode(LED_red, OUTPUT);
+  pinMode(LED_green, OUTPUT);
+
+
+}
+
+// LED's stuff
+void loop()(
+  
+)
+
+// Initialize the Stepper Motor
   stepper(stepper_revs, 22,24,26,28);
 
 // Initialize the DHT sensor
@@ -17,22 +43,22 @@ DHT dht(DHTPIN, DHTTYPE);
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//stepper motor stuff
+//Stepper Motor stuff
   void steup(){
     stepper. setspeed(50);
 
   }
 
 void loop(){
-  // clockwise
+
   stepper.step(stepper_revs);
   delay(500);
 
-// counterclockwise
+
   stepper.step(-stepper_revs);
   delay(500);
 }
-// LCD STUFF 
+// LCD stuff 
 void setup() {
   dht.begin();
   
@@ -47,7 +73,6 @@ void loop() {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
 
-  // Check if any reading failed
   if (isnan(temperature) || isnan(humidity)) {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -68,5 +93,5 @@ void loop() {
   lcd.print(humidity);
   lcd.print("%");
 
-  delay(1000);
+  delay(500);
 }
